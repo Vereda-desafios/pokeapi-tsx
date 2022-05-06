d6b55125bfd6fbca1029f48f59d7f74c73a351d4
mornieur <mariafernanda-coro@hotmail.com>
2022-05-06 13:31:07 -0300

    Adicionando fundo

--- src/components/PokeCard/styles.ts
+++ src/components/PokeCard/styles.ts
@@ -1,7 +1,19 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    display: flex;
+background: #7b68ee;
+margin-bottom: 20px;
+padding: 15px;
+display: flexbox;
+width: 250px;
+height: 300px;
+cursor: pointer;
+transform: scale(0.9);
+border-color: #7b68ee;
+border-width: 2px;
+border-style: solid;
+border-radius: 30px;
+transition: all ease 0.2s;
 `
 
 export const Card = styled.div`
