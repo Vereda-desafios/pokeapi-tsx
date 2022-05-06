237e0062ee59b47a8bf517843310e260073a7111
mornieur <mariafernanda-coro@hotmail.com>
2022-05-06 12:19:16 -0300

    adicionando commit

---
+++ src/Pages/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import * as C from './styles'
+
+export const Header = () => {
+    <C.Container>
+        <h1>PokeApi</h1>
+    </C.Container>
+}
---
+++ src/services/App.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { PokeCard } from '../components/PokeCard'
+import { Header } from '../Pages/Header'
+
+interface Repository {
+  name: string;
+  url: string;
+  description: string;
+  
+}
+
+function App() {
+  const [repositories, setRepositories] = useState<any>([{}])
+
+  useEffect(() => {
+    fetch(' https://pokeapi.co/api/v2/pokemon/?offset=151&limit=302')
+    .then(response => response.json())
+    .then(data => {
+      console.log('estou aqui', data.results)
+      setRepositories([...data.results])
+      
+    })
+  },[])
+  
+  return (
+    <ul>
+      {
+        repositories.length !==0 && repositories?.map((repo: { name: string, url: string}) => {
+          return (
+            <li key={repo.name}>
+               <PokeCard props={repo}/>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+export default App;
