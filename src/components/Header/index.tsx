28015536da7853e32853a0ea52399bf04949e530
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-05-09 12:13:33 -0300

    criando input de busca

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { Container, PokedexBackground } from './styles'
+import { Container, PokedexBackground, SearchInputContainer } from './styles'
 import pokeballImage from '../../assets/pokeball.png'
+import searchIcon from '../../assets/magnifying-glass.png'
 
 export function Header() {
   return (
@@ -7,6 +8,12 @@ export function Header() {
       <PokedexBackground>
         <img src={pokeballImage} alt="Pokebola centralizada no header" />
       </PokedexBackground>
+      <SearchInputContainer>
+        <input type="text" id="searchIcon" />
+        <label htmlFor="searchIcon">
+          <img src={searchIcon} alt="Lupa para buscar pokemon" />
+        </label>
+      </SearchInputContainer>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -21,3 +21,26 @@ export const PokedexBackground = styled.div`
     width: 70px;
   }
 `
+
+export const SearchInputContainer = styled.div`
+  width: 450px;
+  height: 35px;
+  box-shadow: 1px 2px 5px 1px rgba(0, 0, 0, 0.2);
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 8px;
+  margin: 28px auto;
+
+  input {
+    width: 100%;
+    height: 30px;
+    border: 0;
+    font-size: 18px;
+
+    &:focus {
+      outline: none;
+    }
+  }
+`
